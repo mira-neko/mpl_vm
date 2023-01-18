35ace75f6cac5be49708985a3fba4eebfad0e3d9
miralushch <miralushch@gmail.com>
2023-01-19 06:56:45 +0700

    some refacroring & making a program iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::{io::{stdin, stdout, Write}, fmt};
+use std::{
+    fmt,
+    io::{stdin, stdout, Write},
+};
 
 #[derive(Debug, Clone)]
 pub enum Instructions {
@@ -29,11 +32,26 @@ pub enum Instructions {
     Jmpa,
     Jiza,
     Jnza,
-    Ret
+    Ret,
 }
 
 #[derive(Debug, Clone)]
-pub struct Program(Vec<Instructions>);
+pub struct Program {
+    program: Vec<Instructions>,
+    ip: usize,
+    stack: Vec<f64>,
+    call_stack: Vec<usize>,
+    array: [f64; 256],
+    ap: u8,
+    debug: bool,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    PopFromEmptyStack,
+    PopFromEmptyCallStack,
+    InputFailed,
+}
 
 impl fmt::Display for Instructions {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -80,27 +98,31 @@ impl Instructions {
         call_stack: &mut Vec<usize>,
         array: &mut [f64; 256],
         ap: &mut u8,
-        debug: bool
-    ) -> Option<()> {
+        debug: bool,
+    ) -> Result<Option<f64>, Error> {
         use Instructions::*;
 
         match self {
             Psh(n) => stack.push(*n),
             Pfa => stack.push(array[*ap as usize]),
-            Pta => array[*ap as usize] = stack.pop()?,
+            Pta => array[*ap as usize] = stack.pop().ok_or(Error::PopFromEmptyStack)?,
             Gap => stack.push(*ap as f64),
-            Ptap => *ap = stack.pop()? as u8,
+            Ptap => *ap = stack.pop().ok_or(Error::PopFromEmptyStack)? as u8,
             Sap(n) => *ap = *n,
-            Pek => println!("{}", stack.last()?),
+            Pek => return Some(stack.last().copied().ok_or(Error::PopFromEmptyStack)).transpose(),
             Inp => {
                 let mut input = String::new();
                 print!("enter number: ");
-                stdout().flush().ok()?;
-                stdin().read_line(&mut input).ok()?;
-                stack.push(input[0..(input.len()-1)].parse::<f64>().ok()?)
+                stdout().flush().or(Err(Error::InputFailed))?;
+                stdin().read_line(&mut input).or(Err(Error::InputFailed))?;
+                stack.push(
+                    input[0..(input.len() - 1)]
+                        .parse::<f64>()
+                        .or(Err(Error::InputFailed))?,
+                )
             }
-            Dup => stack.push(*stack.last()?),
-            Pop => stack.pop().map(|_| ())?,
+            Dup => stack.push(*stack.last().ok_or(Error::PopFromEmptyStack)?),
+            Pop => stack.pop().map(|_| ()).ok_or(Error::PopFromEmptyStack)?,
             Swp => {
                 let len = stack.len();
                 stack.swap(len - 1, len - 2)
@@ -110,42 +132,42 @@ impl Instructions {
                 stack.swap(len - 1, len - *n - 2)
             }
             Add => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst + snd)
             }
             Sub => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst - snd)
             }
             Mul => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst * snd)
             }
             Div => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst / snd)
             }
             Mod => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst % snd)
             }
             Abs => {
-                let n = stack.pop()?;
+                let n = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(n.abs())
             }
             Max => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst.max(snd))
             }
             Min => {
-                let snd = stack.pop()?;
-                let fst = stack.pop()?;
+                let snd = stack.pop().ok_or(Error::PopFromEmptyStack)?;
+                let fst = stack.pop().ok_or(Error::PopFromEmptyStack)?;
                 stack.push(fst.min(snd))
             }
             Jmp(n) => {
@@ -153,38 +175,36 @@ impl Instructions {
                 *ip = n.overflowing_sub(1).0
             }
             Jiz(n) => {
-                if stack.pop()?.abs() <= 1E-7 {
+                if stack.pop().ok_or(Error::PopFromEmptyStack)?.abs() <= 1E-7 {
                     call_stack.push(*ip);
                     *ip = n.overflowing_sub(1).0
                 }
             }
             Jnz(n) => {
-                if stack.pop()?.abs() >= 1E-7 {
+                if stack.pop().ok_or(Error::PopFromEmptyStack)?.abs() >= 1E-7 {
                     call_stack.push(*ip);
                     *ip = n.overflowing_sub(1).0
                 }
             }
-            Ipta => {
-                array[*ap as usize] = *ip as f64
-            }
+            Ipta => array[*ap as usize] = *ip as f64,
             Jmpa => {
                 call_stack.push(*ip);
                 *ip = array[*ap as usize] as usize
             }
             Jiza => {
-                if stack.pop()?.abs() <= 1E-7 {
+                if stack.pop().ok_or(Error::PopFromEmptyStack)?.abs() <= 1E-7 {
                     call_stack.push(*ip);
                     *ip = array[*ap as usize] as usize
                 }
             }
             Jnza => {
-                if stack.pop()?.abs() >= 1E-7 {
+                if stack.pop().ok_or(Error::PopFromEmptyStack)?.abs() >= 1E-7 {
                     call_stack.push(*ip);
                     *ip = array[*ap as usize] as usize
                 }
             }
             Ret => {
-                *ip = call_stack.pop()?;
+                *ip = call_stack.pop().ok_or(Error::PopFromEmptyCallStack)?;
             }
         };
         *ip = ip.overflowing_add(1).0;
@@ -193,38 +213,61 @@ impl Instructions {
             println!("instruction: {self:?}\nip: {ip}\nstack: {stack:?}\ncall_stack: {call_stack:?}\narray: {array:?}\nap: {ap}\n");
         }
 
-        Some(())
+        Ok(None)
     }
 }
 
 impl From<Vec<Instructions>> for Program {
     fn from(program: Vec<Instructions>) -> Program {
-        Program(program)
+        Program {
+            program,
+            stack: Vec::new(),
+            call_stack: Vec::new(),
+            array: [0.; 256],
+            ap: 0,
+            ip: 0,
+            debug: false,
+        }
+    }
+}
+
+impl From<(Vec<Instructions>, bool)> for Program {
+    fn from((program, debug): (Vec<Instructions>, bool)) -> Program {
+        Program {
+            program,
+            stack: Vec::new(),
+            call_stack: Vec::new(),
+            array: [0.; 256],
+            ap: 0,
+            ip: 0,
+            debug,
+        }
     }
 }
 
 impl fmt::Display for Program {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for instruction in &self.0 {
-            writeln!(f, "{instruction}")?;
-        }
-
-        Ok(())
+        self.program
+            .iter()
+            .try_for_each(|instruction| writeln!(f, "{instruction}"))
     }
 }
 
-impl Program {
-    pub fn eval(self, debug: bool) -> Option<()> {
-        let mut stack = Vec::new();
-        let mut call_stack = Vec::new();
-        let mut array = [0.; 256];
-        let mut ap = 0;
-        let mut ip = 0;
+impl Iterator for Program {
+    type Item = Result<Option<f64>, Error>;
 
-        while ip < self.0.len() {
-            self.0[ip].eval(&mut ip, &mut stack, &mut call_stack, &mut array, &mut ap, debug)?;
+    fn next(&mut self) -> Option<Result<Option<f64>, Error>> {
+        if self.ip < self.program.len() {
+            Some(self.program[self.ip].eval(
+                &mut self.ip,
+                &mut self.stack,
+                &mut self.call_stack,
+                &mut self.array,
+                &mut self.ap,
+                self.debug,
+            ))
+        } else {
+            None
         }
-
-        Some(())
     }
 }
